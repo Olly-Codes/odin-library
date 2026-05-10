1a5c4f2ca053524635df922217fd3ce250249b18
Olly <ollycodesdev@gmail.com>
2026-05-10 13:19:27 +0200

    Add show all books function
    Allows the user to see all the books in their library

--- main.js
+++ main.js
@@ -17,3 +17,30 @@ function addBook(title, author, pages, synopsis){
     Library.push(book);
     localStorage.setItem("books", JSON.stringify(Library))
 }
+
+function showAllBooks(arr) {
+    if(arr.length === 0) {
+        console.log("No books added...");
+        return;
+    }
+
+    arr.forEach(book => {
+        const card = document.createElement("div");
+        const title = document.createElement("h1");
+        const author = document.createElement("h2");
+        const pages = document.createElement("p");
+        const synopsis = document.createElement("p");
+
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        synopsis.textContent = book.synopsis;
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(synopsis);
+
+        container.appendChild(card);
+    });
+}
