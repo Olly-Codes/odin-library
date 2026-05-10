3db6e24b7863349932eb5dee201dc2b446ebef4a
Olly <ollycodesdev@gmail.com>
2026-05-10 13:16:09 +0200

    Add local storage for books added in library
    This allows the user to view previously saved books

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const Library = [];
+const container = document.querySelector("#container");
+
+const Library = JSON.parse(localStorage.getItem("books")) || [];
 
 function Book(id, title, author, pages, synopsis) {
     this.id = id;
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = `${pages} pages`;
     this.synopsis = synopsis;
 }
 
 function addBook(title, author, pages, synopsis){
     const bookId = crypto.randomUUID();
     const book = new Book(bookId, title, author, pages, synopsis);
+
     Library.push(book);
+    localStorage.setItem("books", JSON.stringify(Library))
 }
